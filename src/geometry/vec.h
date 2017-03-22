19da0b1511bf3dda3c59c7b531760bfc065aff33
Matteo Monti <matteo.monti@rain.vg>
2017-03-22 15:30:53 +0100

    Add scaffolding for vec
    Basic scaffolding for class vec, two-dimensional vector of doubles.

---
+++ src/geometry/vec.h
@@ -0,0 +1,31 @@
+// Forward declarations
+
+class vec;
+
+#if !defined(__forward__) && !defined(__nobb__geometry__vec__h)
+#define __nobb__geometry__vec__h
+
+class vec
+{
+public:
+    // Settings
+    // Nested classes
+private:
+    // Service nested classes
+    // Members
+public:
+    // Constructors
+    // Destructor
+    // Methods
+private:
+    // Private methods
+public:
+    // Operators
+    // Casting
+    // Static members
+    // Static methods
+private:
+    // Private static methods
+};
+
+#endif
