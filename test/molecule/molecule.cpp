ec59463b0ddb566b6828cf0a2450d090715b395d
carlidel <carlidel@ymail.com>
2017-03-25 16:51:33 +0100

    add minor tests for molecule

--- test/molecule/molecule.cpp
+++ test/molecule/molecule.cpp
@@ -19,7 +19,7 @@ TEST_CASE("Molecule initialize correctly and prints output", "[constructors] [pr
 
     a.push_back(atom(vec(3, 4), 1, 1));
     a.push_back(atom(vec(-3, 4), 1, 1));
-    a.push_back(atom(vec(0, 2), 1, 1));
+    a.push_back(atom(vec(0, 2), 1, 2));
 
     std :: cout << a[0] << a[1] << a[2];
 
@@ -29,6 +29,9 @@ TEST_CASE("Molecule initialize correctly and prints output", "[constructors] [pr
 
     std :: cout << m[0] << m[1] << m[2];
 
-    REQUIRE(true == true);
+    REQUIRE(m[0].position().y == 1.);
+    REQUIRE(m[2].position().y == -1.);
+    REQUIRE(m.inertia_moment() == 26.);
+    REQUIRE(m.mass() == 4.);
   }
 }
