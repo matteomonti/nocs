dc936a916fb5cdceebae601845a3eee29e477bee
Matteo Monti <matteo.monti@rain.vg>
2017-05-30 13:23:43 +0200

    Add hashtable.h, hashtable.hpp
    Adds template hashtable structure that allows for O(1)
    insertion, removal, and retrieval, but also for
    efficient, array-based looping through the values
    stored.
    
    We will use this structure to replace the molecule and
    tag sets in engine.

--- src/main.cpp
+++ src/main.cpp
@@ -1,128 +1,35 @@
 #ifdef __main__
 
 #include <iostream>
-#include <unistd.h>
-#include <assert.h>
-#include <iomanip>
 
-#include "engine/engine.hpp"
-#include "molecule/molecule.h"
-#include "graphics/window.h"
-
-
-double rnd1()
-{
-  return ((double) rand()) / RAND_MAX - 0.5;
-}
+#include "data/hashtable.hpp"
 
 int main()
 {
-  srand(0);
+  srand(time(nullptr));
 
-  molecule alpha
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.1, 0.1},
-    {rnd1(), rnd1()},
-    0,
-    M_PI
-  );
+  hashtable <int, int> my_hashtable;
 
-  molecule beta
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.2, 0.2},
-    {rnd1(), rnd1()},
-    0,
-    M_PI/4
-  );
-
-  molecule gamma
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.3, 0.3},
-    {rnd1(), rnd1()},
-    0,
-    M_PI/4
-  );
-
-  molecule delta
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.4, 0.4},
-    {rnd1(), rnd1()},
-    0,
-    M_PI/4
-  );
-
-  molecule epsilon
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.5, 0.5},
-    {rnd1(), rnd1()},
-    0,
-    M_PI/4
-  );
-
-  molecule zeta
-  (
-    {{{0, 0}, 1, 0.025}, {{0.05, 0}, 1, 0.025}, {{0.1, 0}, 1, 0.025}, {{0.15, 0}, 1, 0.025}, {{0, 0.05}, 1, 0.025}, {{0, 0.1}, 1, 0.025}, {{0, 0.15}, 1, 0.025}},
-    {0.6, 0.6},
-    {rnd1(), rnd1()},
-    0,
-    M_PI/4
-  );
-
-  engine engine(4);
-
-  engine.add(alpha);
-  engine.add(beta);
-  engine.add(gamma);
-  engine.add(delta);
-  engine.add(epsilon);
-  engine.add(zeta);
-
-  window my_window;
-
-  for(double t = 0; t <= 10; t += 0.1)
+  for(size_t n = 0;; n++)
   {
-    engine.run(t);
+    if(n % 1000000 == 0)
+      std :: cout << n << std :: endl;
 
-    my_window.clear();
-    my_window.draw(engine);
-    my_window.flush();
+    int keys[16];
 
-    usleep(1.e4);
-  }
+    for(size_t i = 0; i < 16; i++)
+      keys[i] = rand();
 
-  window :: wait_click();
+    for(size_t i = 0; i < 16; i++)
+      my_hashtable.add(keys[i], i);
 
-  molecule eta
-  (
-    {{{0, 0}, 1, 0.025}},
-    {0.5, 0.5},
-    {0., 0.},
-    0.,
-    0.
-  );
+    my_hashtable.remove(keys[0]);
 
-  engine.add(eta);
-
-  my_window.clear();
-  my_window.draw(engine);
-  my_window.flush();
-
-  window :: wait_click();
-
-  for(double t = 10;; t += 0.1)
-  {
-    engine.run(t);
-
-    my_window.clear();
-    my_window.draw(engine);
-    my_window.flush();
-
-    usleep(1.e4);
+    for(size_t i = 15; i >= 1; i--)
+    {
+      assert(my_hashtable[keys[i]] == i);
+      my_hashtable.remove(keys[i]);
+    }
   }
 }
 
